ce2a754c757fe56bfe3361896167b263b78c3a06
Bryce Champaign <bechampaign@protonmail.com>
2020-01-06 16:27:10 -0500

    Add tests for Product Info endpoint and rename to api.test.js
    Tests have been added for the Product Info endpoint (/products/:id) and the file has been renamed as it is testing more than one endpoint.

---
+++ server/tests/api.test.js
@@ -0,0 +1,109 @@
+const chakram = require('chakram'),
+  expect = chakram.expect;
+
+const mockData = require('./mockData');
+
+describe('Product List endpoint', () => {
+  let testData;
+  beforeEach(() => {
+    testData = mockData.listData;
+  });
+  it('Gets correct list of products when no parameters are provided', () => {
+    return chakram.get('http://localhost:3000/products/list').then(response => {
+      expect(response).to.have.status(200);
+      expect(response.body).to.deep.equal(testData.slice(0, 5));
+      return chakram.wait();
+    });
+  });
+
+  it('Gets correct list of products when page parameter is provided', () => {
+    return chakram
+      .get('http://localhost:3000/products/list?page=2')
+      .then(response => {
+        expect(response).to.have.status(200);
+        expect(response.body).to.deep.equal(testData.slice(5, 11));
+        return chakram.wait();
+      });
+  });
+
+  it('Gets correct list of products when count is provided', () => {
+    return chakram
+      .get('http://localhost:3000/products/list?count=2')
+      .then(response => {
+        expect(response).to.have.status(200);
+        expect(response.body).to.deep.equal(testData.slice(0, 2));
+        return chakram.wait();
+      });
+  });
+
+  it('Gets correct list of products when count and page parameters are provided', () => {
+    return chakram
+      .get('http://localhost:3000/products/list?count=2&page=2')
+      .then(response => {
+        expect(response).to.have.status(200);
+        expect(response.body).to.deep.equal(testData.slice(2, 4));
+        return chakram.wait();
+      });
+  });
+
+  it('Responds with 404 if page value is less than 1', () => {
+    return chakram
+      .get('http://localhost:3000/products/list?page=0')
+      .then(response => {
+        expect(response).to.have.status(404);
+        return chakram.wait();
+      });
+  });
+
+  it('Responds with 404 if page value is not a number', () => {
+    return chakram
+      .get('http://localhost:3000/products/list?page=a')
+      .then(response => {
+        expect(response).to.have.status(404);
+        return chakram.wait();
+      });
+  });
+
+  it('Responds with 404 if count value is less than 0', () => {
+    return chakram
+      .get('http://localhost:3000/products/list?count=-1')
+      .then(response => {
+        expect(response).to.have.status(404);
+        return chakram.wait();
+      });
+  });
+
+  it('Responds with 404 if count value is not a number', () => {
+    return chakram
+      .get('http://localhost:3000/products/list?count=a')
+      .then(response => {
+        expect(response).to.have.status(404);
+        return chakram.wait();
+      });
+  });
+});
+
+describe('Product Info endpoint', () => {
+  let testData;
+  beforeEach(() => {
+    testData = mockData.productInfoData;
+  });
+
+  it('Gets the correct product info for an existing product ID', () => {
+    return chakram.get('http://localhost:3000/products/2').then(response => {
+      expect(response).to.have.status(200);
+      expect(response.body).to.deep.equal(testData);
+      expect(response.body.features.length).to.equal(3);
+      return chakram.wait();
+    });
+  });
+
+  it('Reponds 404 when an invalid product ID is entered', () => {
+    return chakram
+      .get('http://localhost:3000/products/1000000000000000000000000000000')
+      .then(response => {
+        expect(response).to.have.status(404);
+        return chakram.wait();
+      });
+  });
+});
